aba51fbe5e42f7e917f73a92f8867fb5f13c1584
lamachina <lucasmaillard@protonmail.com>
2023-10-11 18:19:19 +0200

    get rekt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Container, Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Box, Text, Container, Flex, Heading, useBreakpointValue } from '@chakra-ui/react';
 import { Fade } from '@chakra-ui/transition';
 import CardSlider from './components/Cards/CardSlider';
 import data from './data/data';
@@ -12,7 +12,21 @@ const App = () => {
       <Container maxW="container.xl" p={4}>
         <Fade in={true}>
           <Box borderRadius="md" fontSize={fontSize} w="full" boxShadow="lg" p={5} bg="black" >
-            <CardSlider dataSet={data} />
+            <Heading color='whiteAlpha.900' py={'4rem'}>XCSS</Heading>
+
+            <Text color='whiteAlpha.800'>
+              All we had to draw from. <br></br>
+              Ode to the tangible moments of reality.<br></br>
+              The raw taste for danger.<br></br>
+              Pitiful reward.<br></br>
+              Indulge in the euphoria of a win suggesting that perhaps, in our relentless pursuit of excess, we might just be bound by some universal moral law. <br></br>
+              <br></br>
+              Or maybe, it's all just pretentious drivel.
+            </Text>
+
+            <Text textAlign='end' py={16} color='whiteAlpha.500' >available at Block #811811</Text>
+
+            {/*  <CardSlider dataSet={data} /> */}
           </Box>
         </Fade>
       </Container>
